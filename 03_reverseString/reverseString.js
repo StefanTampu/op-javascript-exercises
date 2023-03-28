3f26986d5b55577272ea02c2854c22a838de9f86
StefanTampu <114590807+StefanTampu@users.noreply.github.com>
2023-03-28 10:21:10 -0400

    Complete 3rd exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split("");
+    let newArray = array.reverse();
+    let newString = newArray.join("");
+    return newString;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
